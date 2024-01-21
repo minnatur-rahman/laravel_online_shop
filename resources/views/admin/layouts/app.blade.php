e8d3df421b675871f408ef3ba6af0086287f7938
millatur <minnatur7373@gmail.com>
2024-01-21 17:30:35 +0600

    edit some

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -11,6 +11,7 @@
 		<!-- Theme style -->
 		<link rel="stylesheet" href="{{asset('admin-assets/css/adminlte.min.css')}}">
 		<link rel="stylesheet" href="{{asset('admin-asssets/css/custom.css')}}">
+        <meta name="csrf-token" content="{{ csrf_token }}">
 	</head>
 	<body class="hold-transition sidebar-mini">
 		<!-- Site wrapper -->
@@ -82,6 +83,14 @@
 		<!-- AdminLTE for demo purposes -->
 		<script src="{{asset('admin-assets/js/demo.js')}}"></script>
 
-        @yield('coustomjs')
+        <script type="text/javascript">
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+        </script>
+
+        @yield('customjs')
 	</body>
 </html>
