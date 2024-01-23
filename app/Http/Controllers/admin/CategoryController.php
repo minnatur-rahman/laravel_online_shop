900ef794c3cdbbe042363703968a9923ee0d4f9d
millatur <minnatur7373@gmail.com>
2024-01-23 10:36:32 +0600

    edit comtroller

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
 {
 
     public function index(){
-        $category = category::latest()->paginate(10);
+        // $categories = category::latest()->paginate(10);
 
         return view('admin.category.list');
     }
