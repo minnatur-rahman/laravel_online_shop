c733a424b243561c91c4401f6a2ea147a411e80f
millatur <minnatur7373@gmail.com>
2024-01-26 09:10:08 +0600

    edit for dinamec

--- resources/views/admin/category/list.blade.php
+++ resources/views/admin/category/list.blade.php
@@ -55,9 +55,15 @@
                                             <td>{{ $catetories->name }}</td>
                                             <td>{{ $catetories->slug }}</td>
                                             <td>
+                                                @if ($categoty->status == 1)
                                                 <svg class="text-success-500 h-6 w-6 text-success" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                                 </svg>
+                                                @else
+                                                <svg class="text-danger h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+													<path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+												</svg>
+                                                @endif
                                             </td>
                                             <td>
                                                 <a href="#">
@@ -74,7 +80,9 @@
                                         </tr>
                                           @endforeach
                                       @else
-
+                                        <tr>
+                                            <td class="colspan-5">Records Not Found</td>
+                                        </tr>
                                       @endif
                                     <tr>
                                         <td>1</td>
